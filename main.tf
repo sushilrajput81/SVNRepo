07dbc4f635c5b2611cae12796a150407232482f9
sushilkumar <sushil.kumar@nagarro.com>
2018-11-30 10:16:04 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+variable "region" {
+  description = "AWS region"
+  default = "us-east-2"
+}
+
+# Setup our aws provider
+provider "aws" {
+  access_key  = "${var.aws_access_key_id}"
+  secret_key  = "${var.aws_secret_access_key}"
+  region      = "${var.region}"
+}
+
+
+#resource "aws_ecr_repository" "sushil" {
+ # name = "docker-ecs"
+
+
+#}
