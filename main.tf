de1c8e2effb02a82a71278fd51469026f65c68c4
sushilkumar <sushil.kumar@nagarro.com>
2018-11-30 12:06:21 +0530

    Code updated

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "region" {
 
 # Setup our aws provider
 provider "aws" {
-  access_key  = "${var.aaws_access_key_id}"
+  access_key  = "${var.aws_access_key_id}"
   secret_key  = "${var.aws_secret_access_key}"
   region      = "${var.region}"
 }
